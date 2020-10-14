2d294199bbb45841e3b0d608447b027df6d4c48c
Liya174 <lusya-1992@mail.ru>
2020-10-14 13:19:55 +0500

    fist day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/header';
+import Main from './components/Main/main';
+import Feature from './components/Feature/feature';
+import Footer from './components/Footer/footer';
+import Calendar from './components/Calendar/calendar';
+import Details from './components/Details/details';
+
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Feature />
+      {/*<Calendar />s
+      <Details /> */}
+      <Footer />
+    </> 
   );
 }
 
